b85bd0a408aac571668d41bc875e79164056a659
Shradha Pulla and Tim Jung <shradha@>
2016-05-31 14:02:34 -0700

    Add animations from jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
+<!--First Animation-->
 $(".clickable").click(function() {
-  $("#initially-shown").toggle();
+  $("#initially-shown").fadeOut();
   $("#initially-hidden").toggle();
-})
+});
+
+$(".close").click(function() {
+  $("#initially-hidden").fadeOut();
+  $("#initially-shown").toggle()
+});
+
+<!--Second Animation-->
+$(".clickable").click(function() {
+  $("#initially-shown").slideToggle();
+  $("#initially-hidden").toggle();
+});
+
+$(".close").click(function() {
+  $("#initially-hidden").slideUp();
+  $("#initially-shown").toggle()
+});
